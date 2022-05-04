c71b5cec8927df847b69edc97e9d36fa15f36c3f
“yusufnoor786” <zuzu950444@gamil.com>
2022-05-04 09:46:42 +0200

    vendor: Work On Version

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -20,10 +20,10 @@ endif
 
 # Versioning props
 ADDITIONAL_SYSTEM_PROPERTIES  += \
-    org.elixir.version=$(Elixir_BASE_VERSION) \
+    org.elixir.version=$(FALCON_BASE_VERSION) \
     org.elixir.version.display=$(CUSTOM_VERSION) \
     org.elixir.build_date=$(CUSTOM_BUILD_DATE) \
     org.elixir.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
     org.elixir.build_type=$(CUSTOM_BUILD_TYPE) \
-    org.elixir.codename=$(Elixir_CODENAME) \
+    org.elixir.codename=$(FALCON_CODENAME) \
     ro.elixir.maintainer=$(Elixir_MAINTAINER)
